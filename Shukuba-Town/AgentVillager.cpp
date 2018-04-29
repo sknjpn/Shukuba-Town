c4437c704e076d3eece8a1694d2527d5b629f2c2
sknjpn <sknjpn@gmail.com>
2018-04-29 21:33:12 +0900

    Villagerの処理修正

--- Shukuba-Town/AgentVillager.cpp
+++ Shukuba-Town/AgentVillager.cpp
@@ -2,6 +2,8 @@
 
 #include "Village.h"
 
+#include "Job.h"
+
 #include "TaskMove.h"
 #include "TaskWait.h"
 
@@ -15,7 +17,14 @@ namespace skn
 
 	void AgentVillager::behave()
 	{
-		add_task(new TaskMove(get_position() + s3d::RandomVec2(100.0)));
-		add_task(new TaskWait(0.1));
+		if (m_job != nullptr)
+		{
+			m_job->work();
+		}
+		else
+		{
+			add_task(new TaskMove(get_position() + s3d::RandomVec2(100.0)));
+			add_task(new TaskWait(0.1));
+		}
 	}
 }
