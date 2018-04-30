3ed5a04ada51b3715655b2f441c4c3de746f8019
sknjpn <sknjpn@gmail.com>
2018-05-01 06:30:49 +0900

    Junctionの実装

--- Shukuba-Town/Junction.cpp
+++ Shukuba-Town/Junction.cpp
@@ -2,5 +2,16 @@
 
 namespace skn
 {
-
+	Junction::Junction(Node* node)
+		: m_node(node)
+		, m_path(nullptr)
+		, m_t(0)
+	{
+	}
+	Junction::Junction(Path* path, double t)
+		: m_node(nullptr)
+		, m_path(path)
+		, m_t(t)
+	{
+	}
 }
--- Shukuba-Town/Junction.h
+++ Shukuba-Town/Junction.h
@@ -4,15 +4,18 @@
 
 namespace skn
 {
+	class Node;
 	class Path;
 
 	class Junction
 		: public Transform
 	{
+		Node*	m_node;
 		Path*	m_path;
 		double	m_t;
 
 	public:
+		Junction(Node* node);
 		Junction(Path* path, double t);
 	};
 }
