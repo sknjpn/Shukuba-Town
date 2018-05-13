ce8b4e3a4abbdf404077a3bd314a40d6e1f0931c
sknjpn <sknjpn@gmail.com>
2018-05-14 05:52:28 +0900

    問題の修正

--- Shukuba-Town/Node.cpp
+++ Shukuba-Town/Node.cpp
@@ -1,4 +1,5 @@
 #include "Node.h"
+#include "Path.h"
 #include "Field.h"
 
 Node::Node(const Vec2& position, double radius)
@@ -11,4 +12,14 @@ Node::Node(const Vec2& position, double radius)
 Node::~Node()
 {
 	g_field->remove_node(this);
+}
+
+void Node::connect(Node * to, double width)
+{
+	m_paths.emplace_back(new Path(this, to, width)); to->m_paths.emplace_back(new Path(to, this, width));
+}
+
+void Node::disconnect(Node * to)
+{
+	m_paths.remove_if([to](Path* p) { return p->get_to() == to; }); to->m_paths.remove_if([this](Path* p) { return p->get_to() == this; });
 }
--- Shukuba-Town/Node.h
+++ Shukuba-Town/Node.h
@@ -15,8 +15,8 @@ public:
 	Node(const Vec2& position, double radius);
 	~Node();
 
-	void connect(Node* to) { m_paths.emplace_back(new Path(this, to)); to->m_paths.emplace_back(new Path(to, this)); }
-	void disconnect(Node* to) { m_paths.remove_if([to](Path* p) { return p->get_to() == to; }); to->m_paths.remove_if([this](Path* p) { return p->get_to() == this; }); }
+	void connect(Node* to, double width);
+	void disconnect(Node* to);
 
 	bool has_path(Node* to) const { return m_paths.end() != std::find_if(m_paths.begin(), m_paths.end(), [to](Path* p) { return p->get_to() == to; }); }
 	Path* get_path(Node* to) const { return *std::find_if(m_paths.begin(), m_paths.end(), [to](Path* p) { return p->get_to() == to; }); }
