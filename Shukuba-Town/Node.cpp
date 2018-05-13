05928557406dbc44457b7d7472ab12ca19d744ca
sknjpn <sknjpn@gmail.com>
2018-05-14 05:53:07 +0900

    フォーマットの修正

--- Shukuba-Town/Node.cpp
+++ Shukuba-Town/Node.cpp
@@ -14,13 +14,13 @@ Node::~Node()
 	g_field->remove_node(this);
 }
 
-void Node::connect(Node * to, double width)
+void Node::connect(Node* to, double width)
 {
 	this->m_paths.emplace_back(new Path(this, to, width)); 
 	to->m_paths.emplace_back(new Path(to, this, width));
 }
 
-void Node::disconnect(Node * to)
+void Node::disconnect(Node* to)
 {
 	this->m_paths.remove_if([to](Path* p) { return p->get_to() == to; }); 
 	to->m_paths.remove_if([this](Path* p) { return p->get_to() == this; });
