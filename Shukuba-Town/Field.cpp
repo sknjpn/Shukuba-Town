2d2d8e4033a56e44f7c29c8950d40da6b4133639
sknjpn <sknjpn@gmail.com>
2018-05-14 05:55:53 +0900

    メンバの実装

--- Shukuba-Town/Field.cpp
+++ Shukuba-Town/Field.cpp
@@ -1,6 +1,7 @@
 #include "Field.h"
 #include "Camera.h"
 #include "Node.h"
+#include "Path.h"
 
 Field* g_field = nullptr;
 
@@ -13,6 +14,28 @@ Field::Field()
 Field::~Field()
 {
 	delete m_camera;
+}
+
+Path* Field::get_closest_path(const Vec2 & position) const
+{
+	auto it = std::min_element(
+		m_paths.begin(),
+		m_paths.end(),
+		[&position](Path* a, Path* b) { return Geometry2D::Distance(a->get_line(), position) < Geometry2D::Distance(b->get_line(), position); }
+	);
+
+	return (it == m_paths.end()) ? nullptr : *it;
+}
+
+Node* Field::get_closest_node(const Vec2 & position) const
+{
+	auto it = std::min_element(
+		m_nodes.begin(),
+		m_nodes.end(),
+		[&position](Node* a, Node* b) { return a->get_position().distanceFrom(position) < b->get_position().distanceFrom(position); }
+	);
+
+	return (it == m_nodes.end()) ? nullptr : *it;
 }
 
 Node* Field::get_node(const Vec2 & position) const
