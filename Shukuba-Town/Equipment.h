3c61b6472e4b413a76b0729870a17e8d88108664
sknjpn <sknjpn@gmail.com>
2018-05-16 04:59:54 +0900

    コードの移植

--- Shukuba-Town/Equipment.h
+++ Shukuba-Town/Equipment.h
@@ -1,14 +1,28 @@
 #pragma once
 
 #include "Transform.h"
-#include "Drawable.h"
+#include "Uncopyable.h"
 
-class Equipment
-	: public Uncopyable
-	, public Transform
-	, public Drawable
+namespace skn
 {
+	class Equipment
+		: public Transform
+		, public Uncopyable
+	{
+		s3d::Polygon	m_base_shape;
+		s3d::Texture	m_texture;	//テクスチャ
 
-public:
-	Equipment();
-};
+	public:
+		Equipment(const Position& position, const Rotation& rotation, s3d::JSONValue json);
+		virtual ~Equipment() = default;
+
+		s3d::Polygon	get_shape() const
+		{
+			return m_base_shape
+				.rotated(get_rotation())
+				.movedBy(get_position());
+		}
+
+		void			draw() const;
+	};
+}
