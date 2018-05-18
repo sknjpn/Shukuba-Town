391d36e5d080c23c7462cabc023265be00814466
sknjpn <sknjpn@gmail.com>
2018-05-19 08:16:24 +0900

    修正

--- Shukuba-Town/Builder_Building.h
+++ Shukuba-Town/Builder_Building.h
@@ -6,39 +6,8 @@
 class Builder_Building
 	: public Builder
 {
-	class Sample
-	{
-		bool		m_is_selected;
-		Texture		m_texture;
-		Polygon		m_base_shape;
-		Polygon		m_base_site;
-		Position	m_entrance;
-		JSONValue	m_json;
-
-	public:
-		Sample(JSONValue json);
-
-		//getter
-		Texture			get_texture() const { return m_texture; }
-		const Polygon&	get_base_shape() const { return m_base_shape; }
-		const Polygon&	get_base_site() const { return m_base_site; }
-		const Position&	get_entrance() const{ return m_entrance; }
-		JSONValue		get_json() const { return m_json; }
-
-		void	set_selected(bool is_selected) { m_is_selected = is_selected; }
-
-		bool	is_clicked(const Vec2& position) const;
-
-		bool	is_mouse_over(const Vec2& position) const;
-
-		void	draw(const Vec2& position);
-	};
-
 	Vec2		m_grabbed_position;
 	double		m_rotation;
-
-	Sample*		m_selected_sample;
-	Array<Sample*>	m_samples;
 
 	bool		can_set() const;
 
--- Shukuba-Town/Builder_Road.h
+++ Shukuba-Town/Builder_Road.h
@@ -7,25 +7,8 @@ class Node;
 class Builder_Road
 	: public Builder
 {
-	class Sample
-	{
-		bool	m_is_selected;
-
-	public:
-		Sample();
-
-		void	set_selected(bool is_selected) { m_is_selected = is_selected; }
-
-		bool	is_clicked(const Vec2& position) const;
-
-		void	draw(const Vec2& position);
-	};
-
 	Vec2	m_from_position;
 	Vec2	m_to_position;
-
-	Sample*		m_selected_sample;
-	Array<Sample*>	m_samples;
 
 	void		set_from_position(const Vec2& position);
 	void		set_to_position(const Vec2& position);
