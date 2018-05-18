f529d13f5d8e6ec01cd81d6c9cc90cbc7ffdb70a
sknjpn <sknjpn@gmail.com>
2018-05-19 08:11:30 +0900

    新しく追加

--- Shukuba-Town/Builder.h
+++ Shukuba-Town/Builder.h
@@ -5,8 +5,13 @@ class Sample;
 class Builder
 	: public Uncopyable
 {
+	Sample*			m_selected_sample;
+	Array<Sample*>	m_samples;
+
 public:
 	virtual ~Builder() = default;
+
+	Sample*	get_selected_sample() const;
 
 	virtual void update() = 0;
 };
