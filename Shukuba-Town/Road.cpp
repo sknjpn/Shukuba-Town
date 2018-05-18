3575ed23b602e26c6b159f272a3f77004c86c456
sknjpn <sknjpn@gmail.com>
2018-05-19 07:14:25 +0900

    Roadの登録

--- Shukuba-Town/Road.cpp
+++ Shukuba-Town/Road.cpp
@@ -1,6 +1,7 @@
 #include "Road.h"
 #include "Node.h"
 #include "Path.h"
+#include "Field.h"
 
 Road::Road(Node* from, Node* to)
 	: m_from(from)
@@ -8,9 +9,11 @@ Road::Road(Node* from, Node* to)
 {
 	m_primary = new Path(from, to);
 	m_secondary = new Path(to, from);
+
+	g_field->add_road(this);
 }
 
 Road::~Road()
 {
-
+	g_field->remove_road(this);
 }
