e42a984faa84221847299e6904bb652224a7e1df
sknjpn <sknjpn@gmail.com>
2018-05-19 07:05:52 +0900

    初期化処理の追加

--- Shukuba-Town/Road.cpp
+++ Shukuba-Town/Road.cpp
@@ -1,5 +1,11 @@
 #include "Road.h"
+#include "Node.h"
+#include "Path.h"
 
 Road::Road(Node* from, Node* to)
+	: m_from(from)
+	, m_to(to)
 {
+	m_primary = new Path(from, to);
+	m_secondary = new Path(to, from);
 }
