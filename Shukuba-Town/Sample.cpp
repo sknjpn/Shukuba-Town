e54d3f3d12797d680b81b200637637f29fc11a14
sknjpn <sknjpn@gmail.com>
2018-05-19 07:45:35 +0900

    Sampleの追加

---
+++ Shukuba-Town/Sample.cpp
@@ -0,0 +1,16 @@
+#include "Sample.h"
+
+Sample::Sample(const Rect& rect, Texture texture)
+	: m_rect(rect)
+	, m_texture(texture)
+{
+
+}
+
+void Sample::update()
+{
+}
+
+void Sample::draw()
+{
+}
--- Shukuba-Town/Sample.h
+++ Shukuba-Town/Sample.h
@@ -2,5 +2,18 @@
 
 class Sample
 {
+	bool	m_is_selected;
+	Rect	m_rect;
+	Texture m_texture;
 
+public:
+	Sample(const Rect& rect, Texture texture);
+
+	void	set_selected(bool is_selected) { m_is_selected = is_selected; }
+
+	bool	is_left_clicked() const { return m_rect.leftClicked(); }
+
+	void	update();
+
+	void	draw();
 };
