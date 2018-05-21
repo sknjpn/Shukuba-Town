e11156815a28011e631a0597c1d5ff3d8349c51b
sknjpn <sknjpn@gmail.com>
2018-05-22 06:20:20 +0900

    更新

--- Shukuba-Town/Button.cpp
+++ Shukuba-Town/Button.cpp
@@ -11,5 +11,8 @@ Button::Button(const RectF& rect, Texture texture)
 void Button::draw()
 {
 	m_texture.resized(m_rect.size).draw(m_rect.pos);
+
 	m_rect.drawFrame(2.0, Palette::Black);
+
+	m_rect.draw(ColorF(1.0, m_rect.mouseOver() ? 0.5 : 0.0));
 }
