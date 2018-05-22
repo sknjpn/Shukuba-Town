d99a96fa150b83aea35458f2734ea143bcda18d4
sknjpn <sknjpn@gmail.com>
2018-05-23 04:58:13 +0900

    修正

--- Shukuba-Town/Button_Building.cpp
+++ Shukuba-Town/Button_Building.cpp
@@ -1,5 +1,6 @@
 #include "Button_Building.h"
-
+#include "Builder_Building.h"
+#include "Field.h"
 
 Button_Building::Button_Building(const RectF& rect, Texture texture)
 	: Button(rect, texture)
@@ -14,4 +15,5 @@ Button_Building::~Button_Building()
 
 void Button_Building::on_clicked()
 {
+	g_field->set_builder(new Builder_Building());
 }
