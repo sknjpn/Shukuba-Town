9e1967e7ce033094a9c08d7364d31fb7787c55a3
sknjpn <sknjpn@gmail.com>
2018-05-23 05:33:02 +0900

    修正

--- Shukuba-Town/Menu.cpp
+++ Shukuba-Town/Menu.cpp
@@ -7,14 +7,17 @@
 
 Menu::Menu()
 {
-	m_buttons.emplace_back(new Button_Road(RectF(32 + 0 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(0 * 32, 0, 32, 32))));
-	m_buttons.emplace_back(new Button_Road(RectF(32 + 1 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(1 * 32, 0, 32, 32))));
-	m_buttons.emplace_back(new Button_Building(RectF(32 + 2 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(2 * 32, 0, 32, 32))));
-	m_buttons.emplace_back(new Button_Building(RectF(32 + 3 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(3 * 32, 0, 32, 32))));
-	m_buttons.emplace_back(new Button_Building(RectF(32 + 4 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(4 * 32, 0, 32, 32))));
-	m_buttons.emplace_back(new Button_Building(RectF(32 + 5 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(5 * 32, 0, 32, 32))));
-	m_buttons.emplace_back(new Button_Building(RectF(32 + 6 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(6 * 32, 0, 32, 32))));
-	m_buttons.emplace_back(new Button_Quit(RectF(32 + 7 * 64, 32, 64, 64), Texture(Image(U"data/menu.png").clipped(7 * 32, 0, 32, 32))));
+	auto image = Image(U"data/menu.png");
+	auto json = JSONReader(U"data/data.json");
+
+	m_buttons.emplace_back(new Button_Road(RectF(32 + 0 * 64, 32, 64, 64), Texture(image.clipped(0 * 32, 0, 32, 32))));
+	m_buttons.emplace_back(new Button_Road(RectF(32 + 1 * 64, 32, 64, 64), Texture(image.clipped(1 * 32, 0, 32, 32))));
+	m_buttons.emplace_back(new Button_Building(RectF(32 + 2 * 64, 32, 64, 64), Texture(image.clipped(2 * 32, 0, 32, 32)), json[U"buildings"].arrayView()[0]));
+	m_buttons.emplace_back(new Button_Building(RectF(32 + 3 * 64, 32, 64, 64), Texture(image.clipped(3 * 32, 0, 32, 32)), json[U"buildings"].arrayView()[1]));
+	m_buttons.emplace_back(new Button_Building(RectF(32 + 4 * 64, 32, 64, 64), Texture(image.clipped(4 * 32, 0, 32, 32)), json[U"buildings"].arrayView()[2]));
+	m_buttons.emplace_back(new Button_Building(RectF(32 + 5 * 64, 32, 64, 64), Texture(image.clipped(5 * 32, 0, 32, 32)), json[U"buildings"].arrayView()[3]));
+	m_buttons.emplace_back(new Button_Building(RectF(32 + 6 * 64, 32, 64, 64), Texture(image.clipped(6 * 32, 0, 32, 32)), json[U"buildings"].arrayView()[4]));
+	m_buttons.emplace_back(new Button_Quit(RectF(32 + 7 * 64, 32, 64, 64), Texture(image.clipped(7 * 32, 0, 32, 32))));
 }
 
 Menu::~Menu()
