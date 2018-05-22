8b18f48de99390b3e11696b8ef4b7614fffdc0b0
sknjpn <sknjpn@gmail.com>
2018-05-23 05:15:58 +0900

    修正

--- Shukuba-Town/Builder_Building.h
+++ Shukuba-Town/Builder_Building.h
@@ -3,11 +3,16 @@
 #include "Builder.h"
 #include "Transform.h"
 
+struct Model
+{
+	Texture		m_texture;
+};
+
 class Builder_Building
 	: public Builder
 {
-	Vec2		m_grabbed_position;
-	double		m_rotation;
+	Position	m_grabbed_position;
+	Rotation	m_rotation;
 
 	bool		can_set() const;
 
