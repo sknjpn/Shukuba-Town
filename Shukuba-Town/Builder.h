6fd187157ca7e8a0e7a62bc75315b761ff57d564
sknjpn <sknjpn@gmail.com>
2018-05-23 05:04:45 +0900

    問題の修正

--- Shukuba-Town/Builder.h
+++ Shukuba-Town/Builder.h
@@ -5,15 +5,8 @@ class Sample;
 class Builder
 	: public Uncopyable
 {
-	Sample*			m_selected_sample;
-	Array<Sample*>	m_samples;
-
 public:
 	virtual ~Builder() = default;
 
-	void add_sample(Sample* sample) { m_samples.emplace_back(sample); }
-	Sample*	get_selected_sample() const;
-
-	void update_samples();
 	virtual void update() = 0;
 };
