de7afbef7dc2df584b0f467f89794860266e5e44
sknjpn <sknjpn@gmail.com>
2018-05-23 05:48:16 +0900

    更新

--- Shukuba-Town/Builder_Building.cpp
+++ Shukuba-Town/Builder_Building.cpp
@@ -67,58 +67,55 @@ void Builder_Building::update()
 
 	if (g_field->get_menu()->any_mouse_over()) { return; }
 
-	if (Cursor::PosF().y < Window::Size().y - 80)
+	//Transformer2D
+	auto t = g_field->get_camera()->create_transformer();
+
+	if (MouseR.down())
 	{
-		//Transformer2D
-		auto t = g_field->get_camera()->create_transformer();
+		m_grabbed_position = Cursor::PosF();// .movedBy(Vec2::Left(48.0).rotated(m_rotation));
+	}
 
-		if (MouseR.down())
-		{
-			m_grabbed_position = Cursor::PosF();// .movedBy(Vec2::Left(48.0).rotated(m_rotation));
-		}
+	if (MouseR.pressed())
+	{
+		auto vector = Cursor::PosF() - m_grabbed_position;
 
-		if (MouseR.pressed())
-		{
-			auto vector = Cursor::PosF() - m_grabbed_position;
+		m_rotation = atan2(vector.y, vector.x);
+	}
 
-			m_rotation = atan2(vector.y, vector.x);
-		}
+	if (MouseL.down() && can_set())
+	{
+		auto position = Cursor::PosF();
+		auto* node = g_field->get_node(position);
 
-		if (MouseL.down() && can_set())
-		{
-			auto position = Cursor::PosF();
-			auto* node = g_field->get_node(position);
+		if (node == nullptr) { node = new Node(position); }
 
-			if (node == nullptr) { node = new Node(position); }
+		new Building(node, m_rotation, m_model.m_json);
+	}
 
-			new Building(node, m_rotation, m_model.m_json);
-		}
+	{
+		auto color = can_set() ? Palette::Green : Palette::Red;
+		auto position = get_setting_position();
 
-		{
-			auto color = can_set() ? Palette::Green : Palette::Red;
-			auto position = get_setting_position();
+		m_model.m_texture
+			.rotated(m_rotation)
+			.drawAt(get_setting_position(), ColorF(color, 0.50));
 
-			m_model.m_texture
-				.rotated(m_rotation)
-				.drawAt(get_setting_position(), ColorF(color, 0.50));
+		m_model.m_base_shape
+			.rotated(m_rotation)
+			.movedBy(position).drawFrame(1, ColorF(color, 0.50));
 
-			m_model.m_base_shape
-				.rotated(m_rotation)
-				.movedBy(position).drawFrame(1, ColorF(color, 0.50));
+		m_model.m_base_site
+			.rotated(m_rotation)
+			.movedBy(position).drawFrame(1, ColorF(color, 0.25));
+	}
 
-			m_model.m_base_site
-				.rotated(m_rotation)
-				.movedBy(position).drawFrame(1, ColorF(color, 0.25));
-		}
+	if (MouseR.pressed())
+	{
+		Circle(m_grabbed_position, 12.0)
+			.drawFrame(4.0);
 
-		if (MouseR.pressed())
-		{
-			Circle(m_grabbed_position, 12.0)
-				.drawFrame(4.0);
-
-			Line(m_grabbed_position, Cursor::PosF())
-				.drawArrow(4.0, Vec2(16.0, 16.0));
-		}
+		Line(m_grabbed_position, Cursor::PosF())
+			.drawArrow(4.0, Vec2(16.0, 16.0));
 	}
 }
 
