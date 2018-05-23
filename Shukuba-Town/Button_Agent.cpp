6a4098d5809a46f9d54624ecb8a0ff5d5fc857ca
sknjpn <sknjpn@gmail.com>
2018-05-24 05:36:22 +0900

    update

--- Shukuba-Town/Button_Agent.cpp
+++ Shukuba-Town/Button_Agent.cpp
@@ -1,16 +1,18 @@
-#include "stdafx.h"
 #include "Button_Agent.h"
+#include "Field.h"
 
-
-Button_Agent::Button_Agent()
+Button_Agent::Button_Agent(const RectF& rect, Texture texture)
+	: Button(rect, texture)
 {
-}
 
+}
 
 Button_Agent::~Button_Agent()
 {
+
 }
 
 void Button_Agent::on_clicked()
 {
+
 }
--- Shukuba-Town/Button_Agent.h
+++ Shukuba-Town/Button_Agent.h
@@ -6,7 +6,7 @@ class Button_Agent
 	: public Button
 {
 public:
-	Button_Agent(const RectF& rect, Texture texture;
+	Button_Agent(const RectF& rect, Texture texture);
 	~Button_Agent();
 
 	void on_clicked() override;
