a5c60acc96e3c2e589428e26c84b989388cd04ba
sknjpn <sknjpn@gmail.com>
2018-05-24 05:37:03 +0900

    add Builder_Agent

---
+++ Shukuba-Town/Builder_Agent.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Builder_Agent.h"
+
+
+Builder_Agent::Builder_Agent()
+{
+}
+
+
+Builder_Agent::~Builder_Agent()
+{
+}
---
+++ Shukuba-Town/Builder_Agent.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Builder.h"
+class Builder_Agent :
+	public Builder
+{
+public:
+	Builder_Agent();
+	~Builder_Agent();
+};
+
